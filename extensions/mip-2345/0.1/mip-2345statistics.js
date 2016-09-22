26581a818a250b6d9cb193d1e07b885243167e71
顾文琦 <guwq@2345.com>
2016-09-22 11:31:47 +0800

    update for https

--- extensions/mip-2345/0.1/mip-2345statistics.js
+++ extensions/mip-2345/0.1/mip-2345statistics.js
@@ -1,6 +1,6 @@
 define('mip-2345statistics',function () {
     var customElem = require('customElement').create();
-    require('http://union2.50bang.org/js/tianqi');
+    require('//union2.50bang.org/js/tianqi');
     return customElem;
 });
 require(["mip-2345statistics"], function(hotcity) {
